d995a67a5bae9a14cd917732d723fc594b622419
hvnwntw8 <ryan.m.lee89@gmail.com>
2019-11-12 13:22:59 -0700

    added animations to sidebar links

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,6 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "gatsby"
+import { FaNewspaper } from 'react-icons/fa';
+import { FaAddressCard } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
+
+
+
 
 const Container = styled.div`
   flex: 0 0 18%;
@@ -16,18 +22,55 @@ const SideNav = styled.ul`
   margin-top: 3.5rem;
 `
 const SideNavItem = styled.li`
+  position: relative;
 
+  &:not(:last-child) {
+    margin-bottom: 0.5rem;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 3px;
+    background-color: orangered;
+    transform: scaleY(0);
+    transition: transform .2s,
+                width .4s cubic-bezier(1,0,0,1) .2s;
+            
+  }
+
+  &:hover::before {
+    transform: scaleY(1);
+    width: 100%;
+  }
 `
+
+const Icon = styled.svg`
+  width: 3rem;
+  height: 3rem;
+  margin-right: 1.5rem;
+  fill: currentColor;
+`
+
 const StyledLink = styled(Link)`
   text-decoration: none;
   font-weight: 700;
+  font-size: 2rem;
   color: white;
   display: block;
   padding: 1rem 3rem;
+  z-index: 10;
+  position: relative;
   margin-right: 1rem;
   &:hover {
     color: black;
   }
+
+  display: flex;
+  align-items: center;
 `
 
 const Legal = styled.div`
@@ -40,13 +83,22 @@ const Sidebar = () => (
   <Container>
     <SideNav>
       <SideNavItem>
-        <StyledLink to="/">Articles</StyledLink>
+        <StyledLink to="/">
+          <Icon><FaNewspaper size={32}/></Icon>
+          Articles
+        </StyledLink>
       </SideNavItem>
       <SideNavItem>
-        <StyledLink to="/">About Me</StyledLink>
+        <StyledLink to="/">
+          <Icon><FaAddressCard size={32}/></Icon>
+          About Me
+        </StyledLink>
       </SideNavItem>
       <SideNavItem>
-        <StyledLink to="/">Contact</StyledLink>
+        <StyledLink to="/">
+          <Icon><FaEnvelope size={32}/></Icon>
+          Contact
+        </StyledLink>
       </SideNavItem>
     </SideNav>
     <Legal>&copy; 2019 All rights reserved.</Legal>
