351df5e556c9dd9769490c83fec161c9a309bacf
hvnwntw8 <ryan.m.lee89@gmail.com>
2019-11-15 15:56:19 -0700

    added props of title, subtitle, tags(3), and content to card component

--- src/components/cards/preview-card.js
+++ src/components/cards/preview-card.js
@@ -5,6 +5,185 @@ import { FaStar } from 'react-icons/fa';
 import SecondaryButton from '../buttons/secondary'
 import headshot from '../../images/headshot.jpg'
 
+//
+// const Container = styled.div`
+//   display: flex;
+//   background: white;
+//   border-radius: 10px;
+//   border: 1px solid rgba(0, 0, 0, 0.1);
+//   box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.05);
+//   padding: 3rem;
+//   margin: 1rem;
+//   max-width: 900px;
+//   min-width: 400px;
+//
+//   @media all and (max-width: 1250px) {
+//     /* flex-direction: column-reverse; */
+//   }
+// `
+// const LeftColumn = styled.div`
+//   background-color: white;
+//   flex: 0 0 60%;
+//   display: flex;
+//   flex-direction: column;
+//   margin-right: 1rem;
+//
+// `
+// const RightColumn =  styled.div`
+//   background-color: white;
+//   color: white;
+//   flex: 1 auto;
+//   display: flex;
+//   align-items: start;
+//   justify-content: center;
+//   padding: 2rem;
+//   flex-direction: column;
+//
+//   @media all and (max-width: 1250px) {
+//     padding: 0;
+//     align-items: start;
+//     justify-content: start;
+//
+//   }
+//  `
+// const Header = styled.div`
+//   background-color: white;
+//   color: black;
+// `
+// const Title = styled(Link)`
+//   font-size: 4rem;
+//   text-decoration: none;
+//   color: var(--medium-gray-1);
+//   font-weight: 700;
+//
+//   @media all and (max-width: 800px) {
+//     font-size: 3rem;
+//   }
+//
+//   &:hover {
+//     color: gray;
+//   }
+// `
+//
+// const Subtitle = styled.h3`
+//   font-size: 2rem;
+//   font-weight: 300;
+//
+//   @media all and (max-width: 1250px) {
+//     font-size: 1.75rem;
+//
+//   }
+// `
+// const Meta = styled.div`
+//   background-color: white;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//
+//   @media all and (max-width: 800px) {
+//     flex-direction: column;
+//     display: flex;
+//     align-items: start
+//   }
+//
+// `
+//
+// const Hashtags = styled.div`
+// `
+//
+// const Tag = styled(Link)`
+//   text-decoration: none;
+//   margin: .2rem;
+//   color: var(--primary-blue-1);
+//   font-weight: 700;
+//
+//   &:hover {
+//     color: gray;
+//     cursor: pointer;
+//   }
+// `
+// const Icons = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `
+//
+// const Stars = styled.svg`
+//   width: 3rem;
+//   height: 3rem;
+//   margin-right: 1.5rem;
+//   color: #FADB14;
+//   margin-bottom: -1rem;
+//
+//   @media all and (max-width: 1250px) {
+//     width: 2rem;
+//     height: 2rem;
+//     margin-bottom: 0.4rem;
+//     margin-top: 1rem;
+//
+//   }
+// `
+//
+// const Content = styled.div`
+//   border-bottom: 1px solid var(--light-gray-1);
+//   border-top: 1px solid var(--light-gray-1);
+//   padding: 1rem 0;
+//   margin: 1rem 0;
+//
+//   @media all and (max-width: 700px) {
+//     display: none;
+//   }
+// `
+// const Footer = styled.div`
+//   background-color: white;
+//   padding: 1rem 0;
+//   margin-top: 1rem;
+// `
+// const Image = styled.img`
+//   border-radius: 12px;
+//   margin: 1rem;
+//
+//   @media all and (max-width: 1250px) {
+//     margin: 0;
+//   }
+// `
+//
+// const PreviewCard = props => (
+//   <>
+//     <Container>
+//       <LeftColumn>
+//         <Header>
+//           <Title to="/">My Amends</Title>
+//           <Subtitle>For what it's worth, the realest thing I ever wrote.</Subtitle>
+//           <Meta>
+//             <Hashtags>
+//               <Tag>#Motivation</Tag>
+//               <Tag>#MyStory</Tag>
+//               <Tag>#Entrepraneur</Tag>
+//             </Hashtags>
+//             <Icons>
+//               <Stars><FaStar size={20}/></Stars>
+//               <Stars><FaStar size={20}/></Stars>
+//               <Stars><FaStar size={20}/></Stars>
+//               <Stars><FaStar size={20}/></Stars>
+//             </Icons>
+//           </Meta>
+//         </Header>
+//         <Content>
+//           <p>Dolore magna aliquam erat volutpat ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper. Typi qui nunc nobis videntur parum clari fiant sollemnes in. Feugiat nulla facilisis at vero eros et accumsan et iusto odio. </p>
+//         </Content>
+//         <Footer>
+//           <SecondaryButton>Read More</SecondaryButton>
+//         </Footer>
+//       </LeftColumn>
+//       <RightColumn>
+//         <Image src={headshot}/>
+//       </RightColumn>
+//     </Container>
+//   </>
+// )
+//
+// export default PreviewCard;
 
 const Container = styled.div`
   display: flex;
@@ -21,6 +200,7 @@ const Container = styled.div`
     /* flex-direction: column-reverse; */
   }
 `
+
 const LeftColumn = styled.div`
   background-color: white;
   flex: 0 0 60%;
@@ -53,7 +233,7 @@ const Header = styled.div`
 const Title = styled(Link)`
   font-size: 4rem;
   text-decoration: none;
-  color: black;
+  color: var(--medium-gray-1);
   font-weight: 700;
 
   @media all and (max-width: 800px) {
@@ -91,10 +271,33 @@ const Meta = styled.div`
 const Hashtags = styled.div`
 `
 
-const Tag = styled(Link)`
+const Tag1 = styled(Link)`
   text-decoration: none;
   margin: .2rem;
-  color: blue;
+  color: var(--primary-blue-1);
+  font-weight: 700;
+
+  &:hover {
+    color: gray;
+    cursor: pointer;
+  }
+`
+const Tag2 = styled(Link)`
+  text-decoration: none;
+  margin: .2rem;
+  color: var(--primary-blue-1);
+  font-weight: 700;
+
+  &:hover {
+    color: gray;
+    cursor: pointer;
+  }
+`
+const Tag3 = styled(Link)`
+  text-decoration: none;
+  margin: .2rem;
+  color: var(--primary-blue-1);
+  font-weight: 700;
 
   &:hover {
     color: gray;
@@ -118,14 +321,14 @@ const Stars = styled.svg`
     width: 2rem;
     height: 2rem;
     margin-bottom: 0.4rem;
-    margin-top: 1rem; 
+    margin-top: 1rem;
 
   }
 `
 
 const Content = styled.div`
-  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-  border-top: 1px solid rgba(0, 0, 0, 0.2);
+  border-bottom: 1px solid var(--light-gray-1);
+  border-top: 1px solid var(--light-gray-1);
   padding: 1rem 0;
   margin: 1rem 0;
 
@@ -146,19 +349,18 @@ const Image = styled.img`
     margin: 0;
   }
 `
-
 const PreviewCard = props => (
   <>
     <Container>
       <LeftColumn>
         <Header>
-          <Title to="/">My Amends</Title>
-          <Subtitle>For what it's worth, the realest thing I ever wrote.</Subtitle>
+          <Title to="/">{props.title}</Title>
+          <Subtitle>{props.subtitle}</Subtitle>
           <Meta>
             <Hashtags>
-              <Tag>#Motivation</Tag>
-              <Tag>#MyStory</Tag>
-              <Tag>#Entrepraneur</Tag>
+              <Tag1>#{props.tag1}</Tag1>
+              <Tag2>#{props.tag2}</Tag2>
+              <Tag3>#{props.tag3}</Tag3>
             </Hashtags>
             <Icons>
               <Stars><FaStar size={20}/></Stars>
@@ -169,7 +371,7 @@ const PreviewCard = props => (
           </Meta>
         </Header>
         <Content>
-          <p>Dolore magna aliquam erat volutpat ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper. Typi qui nunc nobis videntur parum clari fiant sollemnes in. Feugiat nulla facilisis at vero eros et accumsan et iusto odio. </p>
+          {props.content}
         </Content>
         <Footer>
           <SecondaryButton>Read More</SecondaryButton>
--- src/components/main.js
+++ src/components/main.js
@@ -12,14 +12,21 @@ const Container = styled.div`
 
   @media all and (max-width: 1250px) {
     padding: 0.5rem;
-    padding-top: 6rem; 
+    padding-top: 6rem;
   }
 `
 
 const Main = () => (
   <Container>
-    <PreviewCard />
-    <PreviewCard />
+    <PreviewCard
+      title="My Ammends"
+      subtitle="The realist thing I ever wrote."
+      tag1="mystory"
+      tag2="motivation"
+      tag3="hustle"
+      content="This is test content inside of the testCard component. The cards grow based upon content within and stay the same height based on the tallest card."
+    />
+
   </Container>
 )
 
